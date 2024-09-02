3616d1ad11f40da191d9da30876488312a982f04
MD. NAFIJUR RAHAMAN <161447357+Nafis115@users.noreply.github.com>
2024-09-03 01:44:40 +0600

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -239,7 +239,7 @@ const loadDonation=()=>{
     .then(data =>{
         console.log(data)
         if(data){
-            data.resutls.forEach(donation => {
+            data.forEach(donation => {
                 const userDonor=document.getElementById("user-donor");
                 const li=document.createElement("li")
                 li.innerHTML=`
