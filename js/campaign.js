f68a9797a97a88600cf5289e95db36331497e0f8
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-09-05 16:41:23 +0600

    campaign sorting

--- js/campaign.js
+++ js/campaign.js
@@ -15,18 +15,29 @@ function showAlert(message) {
   function closeSuccessAlert() {
     document.getElementById("s-customAlert").style.display = "none";
   }
-document.addEventListener('DOMContentLoaded', function() {
-    let currentPage = 1;
-    const campaignsPerPage = 3;
 
-    function fetchCampaigns(type = '', status = 'active') {
+document.addEventListener('DOMContentLoaded', function () {
+
+    function fetchCampaigns(type = '', status = '') {
         fetch(`https://donation-platform-backend-rmqk.onrender.com/api/campaign/list/?type=${type}&status=${status}`)
             .then(response => response.json())
             .then(data => {
-                // console.log(data)
-                updateCampaignList(data);
+                sortCampaigns(data);
             })
             .catch(error => console.error('Error fetching campaigns:', error));
+    }
+
+    function sortCampaigns(data) {
+
+        const sortedData = data.sort((a, b) => {
+            const order = {
+                active: 1,
+                completed: 2
+              
+            };
+            return order[a.status] - order[b.status];
+        });
+        updateCampaignList(sortedData);
     }
 
     function updateCampaignList(data) {
@@ -39,10 +50,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     <img src="${campaign.image}" class="w-full h-48 object-cover" alt="${campaign.title}">
                     <div class="p-4">
                         <h5 class="text-xl font-semibold mb-2 text-gray-800">${campaign.title}</h5>
-                        <p class="text-gray-700 mb-2">${campaign.description.slice(0,50)}...</p>
+                        <p class="text-gray-700 mb-2">${campaign.description.slice(0, 50)}...</p>
                         <p class="text-gray-600 mb-2"><strong>Type:</strong> ${campaign.type}</p>
-                        <p class="text-gray-600 mb-2"><strong>Goal Amount:</strong> ${campaign.goal_amount}
-                         BDT</p>
+                        <p class="text-gray-600 mb-2"><strong>Goal Amount:</strong> ${campaign.goal_amount} BDT</p>
                         <p class="text-gray-600 mb-4"><strong>Status:</strong> ${campaign.status}</p>
                         <a href="./campaign_details.html?id=${campaign.id}" style="text-decoration: none;" class="inline-block px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-700 transition duration-300">Details</a>
                     </div>
@@ -52,18 +62,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-  
-
-    document.getElementById('filter-form').addEventListener('submit', function(event) {
+    document.getElementById('filter-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        
+
         const type = document.getElementById('typeFilter').value;
         const status = document.getElementById('statusFilter').value;
 
         fetchCampaigns(type, status);
-        currentPage = 1; // Reset to first page on new filter
     });
-
 
     // Initial load
     fetchCampaigns();
